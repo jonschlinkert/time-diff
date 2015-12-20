613028b99cf00a5a7ffdcc4ef554c34035fd698f
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-20 09:12:29 -0500

    generate readme

--- example.js
+++ example.js
@@ -1,32 +1,23 @@
 'use strict';
 
-var glob = require('glob');
 var Time = require('./');
 
 var time = new Time();
 time.start('foo');
+console.log(time.end('foo', 's'));
 console.log(time.end('foo', 's', 3));
+console.log(time.end('foo', 'ms', 3));
+console.log(time.end('foo', 'μs'));
+console.log(time.end('foo', 'μs', 2));
+console.log(time.end('foo', 'n', 3));
+console.log(time.end('foo', 'n'));
+console.log(time.end('foo', 3));
 console.log(time.end('foo'));
 
-time = new Time('s', 6);
-time.start('bar');
-time.start('baz');
-console.log(time.end('bar'));
-console.log(time.end('baz'));
-
-
-
-time = new Time('ms', 5);
-time.start('glob');
-glob.sync('node_modules/**/*');
-console.log(time.end('glob'));
-
-
-time = new Time();
-time.start('glob');
-glob.sync('node_modules/**/*');
-glob.sync('node_modules/**/*');
-glob.sync('node_modules/**/*');
-console.log(time.end('glob'));
+// time = new Time('s', 6);
+// time.start('bar');
+// time.start('baz');
+// console.log(time.end('bar'));
+// console.log(time.end('baz'));
 
 
