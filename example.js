18843a24b866941fd0796b347550e4f3caeec1d9
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-13 18:53:10 -0400

    examples

--- example.js
+++ example.js
@@ -1,8 +1,9 @@
 'use strict';
 
+var argv = require('minimist')(process.argv.slice(2));
 var Time = require('./');
 
-var time = new Time();
+var time = new Time(argv);
 time.start('foo');
 console.log(time.end('foo', 's'));
 console.log(time.end('foo', 's', 3));
@@ -20,3 +21,24 @@ console.log(time.end('foo'));
 // console.log(time.end('bar'));
 // console.log(time.end('baz'));
 
+/**
+ * Diffs
+ *
+ * Pass an instance of `time-diff` to `log-time-diff`,
+ * along with a "namespace" to use for diffs
+ */
+
+var diff = time.diff('my-app-name', argv);
+
+/**
+ * Next,
+ */
+
+// do stuff
+diff('after init');
+
+// do more stuff...
+diff('before options');
+
+// then load some options
+diff('after options');
